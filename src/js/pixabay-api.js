1e3e86562f105df9b1e76bbe9a7a818f65dc6c7c
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-03-25 16:06:32 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,11 @@
-// функции для HTTP-запросов.
-
+// функції для HTTP-запитів.
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '42552421-44c442bdd9fc0080a82eeb482';
 
 function searchImages(query) {
   const apiUrl = `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  // отправка запроса
+  // надсилання запиту
   return fetch(apiUrl)
     .then(response => {
       if (!response.ok) {
